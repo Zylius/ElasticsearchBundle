f7267335b3348ca1259375a153522feaaabccd33
Martynas Sudintas <martynas.sudintas@nfq.lt>
2014-08-22 14:24:55 +0300

    added mapping reading using annotations from bundles

---
+++ Annotation/Type.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ *************************************************************************
+ * NFQ eXtremes CONFIDENTIAL
+ * [2013] - [2014] NFQ eXtremes UAB
+ * All Rights Reserved.
+ *************************************************************************
+ * NOTICE: 
+ * All information contained herein is, and remains the property of NFQ eXtremes UAB.
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from NFQ eXtremes UAB.
+ *************************************************************************
+ */
+
+namespace Fox\ElasticsearchBundle\Annotation;
+
+/**
+ * Annotation that can be used to signal to the parser
+ * to check mapping type during the parsing process.
+ *
+ * @Annotation
+ */
+final class Type
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $index;
+
+    /**
+     * @var string
+     */
+    public $analyzer;
+
+    /**
+     * @var string
+     */
+    public $indexAnalyzer;
+
+    /**
+     * @var string
+     */
+    public $searchAnalyzer;
+
+    /**
+     * @var float
+     */
+    public $boost;
+
+    /**
+     * @var bool
+     */
+    public $payloads;
+
+    /**
+     * Filters object null values and name
+     *
+     * @return array
+     */
+    public function filter()
+    {
+        return array_diff_key(
+            array_filter(get_object_vars($this)),
+            array_flip(['name'])
+        );
+    }
+}
--- DependencyInjection/FoxElasticsearchExtension.php
+++ DependencyInjection/FoxElasticsearchExtension.php
@@ -2,6 +2,8 @@
 
 namespace Fox\ElasticsearchBundle\DependencyInjection;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Definition;
@@ -17,6 +19,20 @@ use Symfony\Component\DependencyInjection\Loader;
 class FoxElasticsearchExtension extends Extension
 {
     /**
+     * Annotations to load
+     *
+     * @var array
+     */
+    protected $annotations = ['Document', 'Type'];
+
+    /**
+     * Contains mappings gatherd from bundle documents
+     *
+     * @var array
+     */
+    protected $mappings = [];
+
+    /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
@@ -28,6 +44,7 @@ class FoxElasticsearchExtension extends Extension
         $loader->load('services.yml');
 
         $this->loadElasticsearchServices($config, $container);
+        $this->loadDocumentManagers($config, $container);
     }
 
     /**
@@ -40,6 +57,8 @@ class FoxElasticsearchExtension extends Extension
     {
         foreach ($config['connections'] as $name => $setting) {
             $params = [];
+            $mapping = [];
+
             foreach ($setting['hosts'] as $host) {
                 $params['hosts'][] = $host['host'] . ':' . $host['port'];
             }
@@ -48,12 +67,24 @@ class FoxElasticsearchExtension extends Extension
                 $container->get('es.connection_factory')->addParams($params);
             }
 
+            if (array_key_exists($name, $config['document_managers'])) {
+                foreach ($config['document_managers'][$name]['mappings'] as $bundle) {
+                    $mapping = array_replace_recursive(
+                        $mapping,
+                        $this->getMapping($bundle, $container)
+                    );
+                }
+            }
+
             $service = new Definition(
                 'Fox\ElasticsearchBundle\Service\Connection',
                 [
                     [
                         'index' => $setting['index_name'],
-                        'body' => ['settings' => $setting['settings']]
+                        'body' => [
+                            'settings' => $setting['settings'],
+                            'mappings' => $mapping
+                        ]
                     ]
                 ]
             );
@@ -68,6 +99,21 @@ class FoxElasticsearchExtension extends Extension
     }
 
     /**
+     * Loads document managers
+     *
+     * @param array $config
+     * @param ContainerBuilder $container
+     *
+     * @throws \LogicException Connection not found
+     */
+    protected function loadDocumentManagers($config, ContainerBuilder $container)
+    {
+        foreach ($config['document_managers'] as $manager) {
+            //TODO
+        }
+    }
+
+    /**
      * Returns elasticsearch connection service id
      *
      * @param string $name
@@ -77,5 +123,116 @@ class FoxElasticsearchExtension extends Extension
     protected function getServiceId($name)
     {
         return $name == 'default' ? 'es.connection' : sprintf('es.connection.%s', strtolower($name));
+    }
+
+    /**
+     * Returns directory name in which all documents must be stored in bundles
+     *
+     * @return string
+     */
+    protected function getDocumentsDirectoryName()
+    {
+        return 'Document';
+    }
+
+    /**
+     * Registers annotations to registry so that it could be used by reader
+     */
+    protected function registerAnnotations()
+    {
+        foreach ($this->annotations as $annotation) {
+            AnnotationRegistry::registerFile(__DIR__."/../Annotation/{$annotation}.php");
+        }
+    }
+
+    /**
+     * Retrieves mapping from local cache otherwise runs through bundle files
+     *
+     * @param string $bundle
+     * @param ContainerBuilder$container
+     *
+     * @return array
+     */
+    protected function getMapping($bundle, $container)
+    {
+        if (array_key_exists($bundle, $this->mappings)) {
+            return $this->mappings[$bundle];
+        }
+
+        return $this->mappings[$bundle] = $this->getBundleMapping($bundle, $container);
+    }
+
+    /**
+     * Searches for documents in bundle and tries to read them.
+     * Returns empty array on containing zero documents
+     *
+     * @param string $bundle
+     * @param ContainerBuilder $container
+     *
+     * @return array
+     * @throws \LogicException
+     */
+    private function getBundleMapping($bundle, ContainerBuilder $container)
+    {
+        $kernelBundles = $container->getParameter('kernel.bundles');
+        $this->registerAnnotations();
+        $mappings = [];
+
+        //Checks if bundle is register in kernel
+        if (array_key_exists($bundle, $kernelBundles)) {
+            $bundleReflection = new \ReflectionClass($kernelBundles[$bundle]);
+            $documents = glob(
+                dirname($bundleReflection->getFileName()) .
+                DIRECTORY_SEPARATOR .
+                $this->getDocumentsDirectoryName() .
+                DIRECTORY_SEPARATOR .
+                '*.php'
+            );
+
+            //Loop through documents found in bundle
+            foreach ($documents as $document) {
+                $filename = pathinfo($document, PATHINFO_FILENAME);
+                $documentReflection = new \ReflectionClass(
+                    $bundleReflection->getNamespaceName() . '\\' .
+                    $this->getDocumentsDirectoryName() . '\\' .
+                    $filename
+                );
+
+                $documentMapping = $this->getDocumentMapping($documentReflection);
+                if ($documentMapping == null) {
+                    continue;
+                }
+                $mappings[strtolower($filename)]['properties'] = $documentMapping;
+            }
+        } else {
+            throw new \LogicException("{$bundle} not found.");
+        }
+
+        return $mappings;
+    }
+
+    /**
+     * Gethers annotation data from class
+     *
+     * @param \ReflectionClass $reflectionClass
+     *
+     * @return array|null
+     */
+    private function getDocumentMapping(\ReflectionClass $reflectionClass)
+    {
+        $mapping = [];
+        $reader = new AnnotationReader();
+
+        if ($reader->getClassAnnotation($reflectionClass, 'Fox\ElasticsearchBundle\Annotation\Document')) {
+            /** @var \ReflectionProperty $property */
+            foreach ($reflectionClass->getProperties() as $property) {
+                $type = $reader->getPropertyAnnotation($property, 'Fox\ElasticsearchBundle\Annotation\Type');
+                $mapping[$type->name] = $type->filter();
+            }
+
+            return $mapping;
+        }
+
+        return null;
     }
 }
